bf41d42e0e0217d21520e50dfcb9744ed2a7ec67
Guilherme Yuuta Mimura <guilhermeymimura@gmail.com>
2025-05-19 20:22:07 -0300

    socorro

--- cpf.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('cpf')
-
-input.addEventListener('keypress',() => {
-    let inputlenght = input.ariaValueMax.length
-
-    if (inputlenght === 3 || input.length === 7 ){
-        input.value += "."
-    }
-} )
